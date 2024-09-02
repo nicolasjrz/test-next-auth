5df0f37dbb4db00029a0c5aeca1a4e5cfb4ef193
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-09-02 10:13:10 -0300

    implemeted button in Sidebar

--- src/components/menu/Sidebar.tsx
+++ src/components/menu/Sidebar.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import { Button } from "../ui";
+import { LogoutButton } from "../button/LogoutButton";
 
 export const Sidebar = () => {
 	return (
@@ -15,7 +17,12 @@ export const Sidebar = () => {
 					<Link href={"/admin"}>Admin</Link>
 				</div>
 				<div className="flex h-full items-center ">
-					<Link href={""}>Logout</Link>
+					<Button>
+						<Link href={"/auth/login"}>Login</Link>{" "}
+					</Button>
+				</div>
+				<div className="flex h-full items-center ">
+					<LogoutButton />
 				</div>
 			</div>
 		</div>
