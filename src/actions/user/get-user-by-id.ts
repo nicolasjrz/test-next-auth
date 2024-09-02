8e6aa77c935f157180c6538a274bb54fa43942f8
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-09-02 10:12:12 -0300

    add actions getUserById

---
+++ src/actions/user/get-user-by-id.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export const getUserById = async (id: string) => {
+	try {
+		const user = await prisma.user.findUnique({
+			where: { id },
+		});
+
+		if (!user) return null;
+
+		return user;
+	} catch (error) {
+		console.log(error);
+
+		throw new Error("No se encontro el id");
+	}
+};
