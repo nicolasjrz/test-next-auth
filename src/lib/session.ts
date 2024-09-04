ce2f2e7cf7b67f47adeab74bbacf933d42724c24
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-09-04 11:18:54 -0300

    delete file

--- src/lib/session.ts
+++
@@ -1,7 +0,0 @@
-import { auth } from "@/auth";
-
-export const currentRole = async () => {
-	const session = await auth();
-	console.log("currentRole ------- Session after assigning role:", session?.user.role); // Verificar si el rol está asignado
-	return session?.user?.role;
-};
