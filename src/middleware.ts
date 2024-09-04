812974c42803f25b85e3e9ee8556f61a7d648e53
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-09-04 11:17:45 -0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,30 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
-
-import { DEFAULT_LOGIN_USER_REDIRECT, DEFAULT_LOGIN_ADMIN_REDIRECT, DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes, adminRoutes } from "@/routes";
-
-import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import { redirect } from "next/navigation";
+import { getToken } from "next-auth/jwt";
+import { cookies } from "next/headers";
+import { apiAuthPrefix, authRoutes, publicRoutes, adminRoutes } from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth(async (req) => {
 	// Extrae la URL solicitada
-	const { nextUrl, auth } = req;
-	// Determina si el usuario está autenticado
-	const isLoggedIn = !!req.auth;
+	const { nextUrl } = req;
 
+	// Obtener las cookies de la solicitud actual
+	const clientCookies = cookies().getAll();
 	const token = await getToken({
-		req,
+		req: { headers: { cookie: clientCookies.map((c) => `${c.name}=${c.value}`).join("; ") } },
 		secret: process.env.AUTH_SECRET,
 	});
 
 	const userRole = token?.role; // Asumimos que el rol se guarda en el token
+	const isLoggedIn = !!token; // Verifica si el token existe
 
-	console.log({ userRole });
 	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-
 	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-
 	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+	const isAdminRoute = adminRoutes.includes(nextUrl.pathname);
 
 	if (isApiAuthRoute) {
 		return; // No redirigir, continuar con la petición
@@ -35,59 +32,28 @@ export default auth(async (req) => {
 
 	if (isAuthRoute) {
 		if (isLoggedIn) {
-			return Response.redirect(new URL("/", nextUrl));
+			if (userRole === "user") return NextResponse.redirect(new URL("/", nextUrl));
+			return NextResponse.redirect(new URL("/admin", nextUrl));
+			// return redirect:{}
 		}
-		return null;
+		return; // Permite el acceso a la página de autenticación si no está autenticado
+	}
+
+	if (isAdminRoute) {
+		if (isLoggedIn) {
+			if (userRole === "user") return NextResponse.redirect(new URL("/", nextUrl));
+			// return redirect:{}
+		}
+		return; // Permite el acceso a la página de autenticación si no está autenticado
 	}
 
 	if (!isLoggedIn && !isPublicRoute) {
 		return Response.redirect(new URL("/auth/login", nextUrl));
 	}
 
-	return;
+	return; // Permite que la petición continúe para el resto de las rutas
 });
 export const config = {
 	// Matcher especifica las rutas donde se aplica este middleware
 	matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
-
-// export default auth((req) => {
-
-// 	const { nextUrl } = req;
-
-// 	// Determina si el usuario está autenticado
-// 	const isLoggedIn = !!req.auth;
-
-// 	console.log("IS LOGGEDIN:", isLoggedIn);
-
-// 	// Determina si la ruta es una API relacionada con la autenticación
-// 	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-
-// 	// Verifica si la ruta solicitada es una ruta pública
-// 	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-
-// 	// Verifica si la ruta solicitada es una ruta de autenticación (login, registro, etc.)
-// 	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-
-// 	// Si la ruta es una API de autenticación, no hace nada (permite continuar)
-// 	if (isApiAuthRoute) {
-// 		return null;
-// 	}
-
-// 	// Si la ruta es una ruta de autenticación y el usuario ya está autenticado,
-// 	// redirige al usuario a la página predeterminada después de iniciar sesión
-// 	if (isAuthRoute) {
-// 		if (isLoggedIn) {
-// 			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-// 		}
-// 		return null; // Permite el acceso a la página de autenticación si no está autenticado
-// 	}
-
-//
-// 	if (!isLoggedIn && !isPublicRoute) {
-// 		return Response.redirect(new URL("/auth/login", nextUrl));
-// 	}
-
-// 	// Permite el acceso a la ruta solicitada si ninguna de las condiciones anteriores se cumple
-// 	return null;
-// });
