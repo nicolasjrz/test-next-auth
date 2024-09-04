3d5432761930e3ccc0c976381d9bb3682903585d
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-09-04 11:17:23 -0300

    config nextauth.ds

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -1,26 +1,18 @@
-
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import { Role } from "@/interfaces/user-interface";
-import NextAuth, { type DefaultSession } from "next-auth"
- 
-export type ExtendedUser = DefaultSession["user"] & {
-    role:"owner" | "administrator" | "employee" | "user";
-}
-
 
 declare module "next-auth" {
   interface Session {
     user: {
-      role: Role; // Aquí simplemente declaramos que role es de tipo Role
+      id?: string; // Puedes agregar otros campos aquí si es necesario
+      role: Role; // Asegúrate de que `role` sea opcional si no siempre está presente
     } & DefaultSession["user"];
   }
 }
 
-
-import { JWT } from "next-auth/jwt"
 declare module "next-auth/jwt" {
-    /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
-    interface JWT {
-      /** OpenID ID Token */
-      role:Role
-    }
+  interface JWT {
+    role?: Role; // Asegúrate de que `role` sea opcional si no siempre está presente
   }
+}
