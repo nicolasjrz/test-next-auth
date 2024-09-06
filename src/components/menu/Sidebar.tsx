2a7f96e9ba5cd8980db1b71b5239151cfca1c768
nicolasjrz <nicolasjuarez1234@gmail.com>
2024-09-06 19:20:18 -0300

    implement role and session user in sidebar

--- src/components/menu/Sidebar.tsx
+++ src/components/menu/Sidebar.tsx
@@ -1,29 +1,61 @@
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
-import { Button } from "../ui";
 import { LogoutButton } from "../button/LogoutButton";
+import { Button } from "../ui";
+import { currentRole, currentUser } from "@/hooks/useCurrentRole";
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+	const role = await currentRole();
+	const user = await currentUser();
+
+	// const { data: session } = useSession();
+
+	console.log(user);
+
 	return (
-		<div className="h-16 w-full   bg-red-300  ">
+		<div className="h-16 w-full bg-red-300">
 			<div className="w-full h-full flex justify-center items-center space-x-4">
-				<div className="flex h-full items-center ">
-					<Link href={"/"}>Home</Link>
+				<div className="flex h-full items-center">
+					<Link href="/">Home</Link>
 				</div>
-				<div className="flex h-full items-center ">
-					<Link href={"/contacto"}>Contacto</Link>
+				<div className="flex h-full items-center">
+					<Link href="/contacto">Contacto</Link>
 				</div>
-				<div className="flex h-full items-center ">
-					<Link href={"/admin"}>Admin</Link>
-				</div>
-				<div className="flex h-full items-center ">
-					<Button>
-						<Link href={"/auth/login"}>Login</Link>{" "}
-					</Button>
-				</div>
-				<div className="flex h-full items-center ">
-					<LogoutButton />
-				</div>
+
+				{role === "administrator" ? (
+					<div className="flex h-full items-center">
+						<Link href="/admin">Admin</Link>
+					</div>
+				) : (
+					""
+				)}
+
+				{!user ? (
+					<div className="flex h-full items-center">
+						<Button>
+							<Link href="/auth/login">Login</Link>
+						</Button>
+					</div>
+				) : (
+					<div className="flex h-full items-center">
+						<LogoutButton />
+					</div>
+				)}
+
+				{/* {!isAuthenticated && (
+					<div className="flex h-full items-center">
+						<Button>
+							<Link href="/auth/login">Login</Link>
+						</Button>
+					</div>
+				)}
+
+				{isAuthenticated && (
+					<div className="flex h-full items-center">
+						<LogoutButton />
+					</div>
+				)} */}
 			</div>
 		</div>
 	);
